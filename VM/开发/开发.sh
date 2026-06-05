60d735bcf341f71940875fcca84cb8b627c6581d
桑梓店镇 <wanmeics1980@qq.com>
2026-06-05 18:11:22 +0800

    Add files via upload

--- VM/开发/开发.sh
+++ VM/开发/开发.sh
@@ -9,10 +9,11 @@ if [ $work_dir == 1 ]; then
 mkdir -p ~/公共/开发[QEMU]	~/公共/开发[QEMU]/diff	~/公共/开发[QEMU]/diff/a	~/公共/开发[QEMU]/diff/b
 
 tar -xf qemu-11.0.1.tar.xz -C ~/公共/开发[QEMU]/diff
-cd ~/公共/开发[QEMU]/diff/qemu-11.0.1;	patch -p1 < ../../../../qemu-10.2.2.patch
+cd ~/公共/开发[QEMU]/diff/qemu-11.0.1
+	patch -p1 < ../../../../手动qemu-11.0.1.patch
 
 rm -rf ~/公共/开发[QEMU]/QEMU开发
-cp -r ~/公共/开发[QEMU]/diff/qemu-11.0.1	~/公共/开发[QEMU]/QEMU开发
+cp -r ~/公共/开发[QEMU]/diff/qemu-11.0.1		~/公共/开发[QEMU]/QEMU开发
 fi
 
 ##	##	##	##	##	##	##	##	##	##	##	##	##	##	
@@ -49,7 +50,10 @@ item=$(echo $item | sed  's/\n//g')
 install -D ~/公共/开发[QEMU]/diff/qemu-11.0.1$item	~/公共/开发[QEMU]/diff/a$item
 install -D ~/公共/开发[QEMU]/QEMU开发$item	~/公共/开发[QEMU]/diff/b$item
 
-done;	diff -uprN ~/公共/开发[QEMU]/diff/a ~/公共/开发[QEMU]/diff/b | 
+done	
+
+
+diff -uprN ~/公共/开发[QEMU]/diff/a ~/公共/开发[QEMU]/diff/b | 
 sed -e 's/\/home\/.*\[QEMU\]\/diff\/a/a/g' -e 's/\/home\/.*\[QEMU\]\/diff\/b/b/g' > ~/公共/开发[QEMU]/开发补丁[$(date "+%Y-%m-%d_%H:%M:%S")].patch
 
 
